8ee2faa0909ac3c6a24a0ad7ee6f5c292b002829
AlexBlacksmith <unbloody.alex@gmail.com>
2022-10-01 14:34:11 +0500

    Added file structure and webpack SCSS config

---
+++ webpack.config.js
@@ -0,0 +1,43 @@
+const path = require('path');
+const HTMLWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+
+module.exports = {
+    entry: './src/app.js',
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'bundle.js'
+  },
+    module: {
+        rules: [
+            {
+                test: /\.css$/,
+                use: [
+                    'style-loader',
+                    'css-loader'
+                ]
+            },
+            {
+                test: /\.s[ac]ss$/i,
+                use: [
+                    MiniCssExtractPlugin.loader,
+                    "css-loader",
+                    "sass-loader",
+                ],
+            },
+        ],
+    },
+    plugins: [
+        new HTMLWebpackPlugin({
+            template: './src/index.html',
+        }),
+        new MiniCssExtractPlugin({
+            filename: 'style.css'
+        })
+    ],
+    devServer: {
+        hot: true,
+        open: true
+    }
+
+}
